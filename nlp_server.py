a80bbb738f7eb41fef8a2f1c8232a285b1135a25
Chase Geigle <geigle1@illinois.edu>
2016-07-16 15:40:53 -0500

    Move asset compilation into Python server code itself.

--- nlp_server.py
+++ nlp_server.py
@@ -3,6 +3,8 @@ app = Flask(__name__, static_folder='static/nlp/', static_url_path='')
 import json
 import sys
 
+import coffeescript, sass
+
 from nlp import NLP
 
 @app.route('/')
@@ -13,9 +15,27 @@ def root():
 def nlp_api():
     return app.nlp.run(request.get_json())
 
+def compile_assets():
+    static = app.static_folder
+
+    with open("{}/javascript/index.js".format(static), 'w') as f:
+        infile = "{}/coffeescript/index.coffee".format(static)
+        js = coffeescript.compile_file(infile)
+        f.write(js)
+
+    with open('{}/css/tagged-text.css'.format(app.static_folder), 'w') as f:
+        infile = "{}/scss/tagged-text.scss".format(static)
+        css = sass.compile(filename=infile)
+        f.write(css)
+
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         print("Usage: {} config.toml".format(sys.argv[0]))
         sys.exit(1)
+
+    print('Compiling assets...')
+    compile_assets()
+
+    print('Loading NLP models...')
     app.nlp = NLP(sys.argv[1])
     app.run(debug=True)
--- search_server.py
+++ search_server.py
@@ -3,6 +3,8 @@ app = Flask(__name__, static_folder='static/search/', static_url_path='')
 import json
 import sys
 
+import coffeescript
+
 from searcher import Searcher
 
 @app.route('/')
@@ -13,9 +15,21 @@ def root():
 def search_api():
     return app.searcher.search(request.get_json())
 
+def compile_assets():
+    static = app.static_folder
+
+    with open("{}/javascript/index.js".format(static), 'w') as f:
+        infile = "{}/coffeescript/index.coffee".format(static)
+        js = coffeescript.compile_file(infile)
+        f.write(js)
+
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         print("Usage: {} config.toml".format(sys.argv[0]))
         sys.exit(1)
+
+    print('Compiling assets...')
+    compile_assets()
+
     app.searcher = Searcher(sys.argv[1])
     app.run(debug=True)
