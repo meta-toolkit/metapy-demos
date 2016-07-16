04b5709d0015a0ea628691c1a1c38f2c936046a8
Chase Geigle <geigle1@illinois.edu>
2016-07-16 17:31:35 -0500

    Add configuration files and scripts for demo app deployment.

---
+++ contrib/search_app.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+USER=massung1
+PYENV_ROOT="/home/$USER/.pyenv"
+PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+
+APPDIR="/home/$USER/metapy-demos"
+PIDFILE=tmp/pids/search_app.pid
+CONFIGFILE=contrib/search_app.cfg
+META_CONFIG=search-config.toml
+
+restart() {
+  pushd $APPDIR > /dev/null
+  source meta-pyenv/bin/activate
+  if [ -e $PIDFILE ] && kill -0 $(cat $PIDFILE)
+  then
+    echo "Gunicorn is running, reloading..."
+    kill -HUP $(cat $PIDFILE)
+  else
+    echo "Starting new gunicorn process..."
+    gunicorn -c $CONFIGFILE "search_server:server('$META_CONFIG')"
+  fi
+  echo "Done!"
+  popd > /dev/null
+}
+
+stop() {
+  pushd $APPDIR > /dev/null
+  if [ -e $PIDFILE ] && kill -0 $(cat $PIDFILE)
+  then
+    echo "Stopping gunicorn..."
+    kill -QUIT $(cat $PIDFILE)
+  fi
+  echo "Done!"
+  popd > /dev/null
+}
+
+case "$1" in
+  start)
+    restart
+    ;;
+  restart)
+    restart
+    ;;
+  stop)
+    stop
+    ;;
+  *)
+    echo "Usage: {start|stop|restart}"
+    exit 1
+    ;;
+esac
